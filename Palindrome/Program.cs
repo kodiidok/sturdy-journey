b378b1ad05f0190e0c6b8b272aa39f11da9b9365
Sahan Kodituwakku <sahan.crkodituwakku@gmail.com>
2024-08-26 19:55:00 +0530

    feat: Palindrome program addeed

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Palindrome;
+
+class Program
+{
+    static bool CheckPalindrome(string inputString, string trashSymbol)
+    {
+        // Two pointers to check for palindrome
+        int left = 0;
+        int right = inputString.Length - 1;
+
+        while (left < right)
+        {
+            // Move left pointer to the next valid character
+            while (left < right && trashSymbol.Contains(inputString[left]))
+            {
+                left++;
+            }
+
+            // Move right pointer to the previous valid character
+            while (left < right && trashSymbol.Contains(inputString[right]))
+            {
+                right--;
+            }
+
+            // Check if the current characters are equal
+            if (left < right && inputString[left] != inputString[right])
+            {
+                Console.WriteLine("Is Palindrome: False");
+                return false;
+            }
+
+            // Move both pointers
+            left++;
+            right--;
+        }
+
+        Console.WriteLine("Is Palindrome: True");
+
+        return true;
+    }
+
+
+    static void Main(string[] args)
+    {
+        string inputString1 = "a@b!!b$a";
+        string trashSymbolsString1 = "!@$";
+
+        string inputString2 = "?Aa#c";
+        string trashSymbolsString2 = "#?";
+
+        CheckPalindrome(inputString1, trashSymbolsString1);
+        CheckPalindrome(inputString2, trashSymbolsString2);
+    }
+}
